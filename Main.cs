bed93f93a0de32ba4320140730812738b793e2be
Yassen Bantchev <yassenb@gmail.com>
2013-10-26 11:58:15 +0300

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace upg
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("209B");
+        }
+    }
+}
---
+++ api/Entities.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+
+namespace upg.api
+{
+    public interface Entities
+    {
+        void AddRoom();
+    }
+}
