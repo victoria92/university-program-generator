30f0cc58a7d87e6199ec9b7ebb07809b33f6d782
David Tankov <lordmordevil@gmail.com>
2013-10-26 15:59:11 +0300

    Refactor requirement classes

---
+++ UniProgramGen/Data/Requirement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace UniProgramGen.Data
+{
+    public class Requirement
+    {
+        public readonly double weight;
+        public readonly List<TimeSlot> availableTimeSlots;
+        public readonly List<Room> availableRooms;
+
+        public Requirement(double weight, List<TimeSlot> availableTimeSlots, List<Room> availableRooms)
+        {
+            this.weight = weight;
+            this.availableTimeSlots = availableTimeSlots;
+            this.availableRooms = availableRooms;
+        }
+    }
+}
