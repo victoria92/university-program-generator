afb0156238461abfd76904259f71a98faa24c76e
Valentin Aitken <bostko@gmail.com>
2013-10-26 18:57:10 +0300

    Added iCal library

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -9,27 +9,51 @@ namespace UnitTests
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        private List<RoomType> organic_room_types;
+        private List<TimeSlot> available_timeslots;
+        private List<Room> rooms;
+        private List<Requirements> requirements_list;
+        private List<Teacher> teachers;
+
         public void initalizeExampleData()
         {
-            var organic_room_types = new List<RoomType>();
-            organic_room_types.Add(RoomType.Projector);
-            organic_room_types.Add(RoomType.Komin);
+            if(organic_room_types == null)
+            {
+                organic_room_types = new List<RoomType>();
+                organic_room_types.Add(RoomType.Projector);
+                organic_room_types.Add(RoomType.Komin);
+            }
 
-            var available_timeslot = new TimeSlot(DayOfWeek.Monday, 9, 22);
-            var available_timeslots = new List<TimeSlot>();
-            available_timeslots.Add(available_timeslot);
+            if(available_timeslots == null)
+            {
+                var available_timeslot = new TimeSlot(DayOfWeek.Monday, 9, 22);
+                available_timeslots = new List<TimeSlot>();
+                available_timeslots.Add(available_timeslot);
+            }
             
-            var room = new Room(organic_room_types, 150, available_timeslots, "ХФ210");
-            var rooms = new List<Room>();
-            rooms.Add(room);
+            if(rooms == null)
+            {
+                var room = new Room(organic_room_types, 150, available_timeslots, "ХФ210");
+                rooms = new List<Room>();
+                rooms.Add(room);
+            }
 
-            var organic_chemistry_room_requirements = new Requirements(10, available_timeslots, rooms);
-            var organich_chemistry_teacher = new Teacher(organic_chemistry_room_requirements, "Prof. Hristo Hristov");
-
+            if(requirements_list == null)
+            {
+                var requirements = new Requirements(10, available_timeslots, rooms);
+                requirements_list = new List<Requirements>();
+                requirements_list.Add(requirements);
+                var organich_chemistry_teacher = new Teacher(requirements_list[0], "Prof. Hristo Hristov");
+            }
             var organic_chemistry_teachers = new List<Teacher>();
 
             var s = new Subject(organic_room_types, organic_chemistry_teachers, "Organic Chemistry Lectures", 1);
+        }
+
+        [TestMethod]
+        public void generateCalendar()
+        {
+
         }
     }
 }
