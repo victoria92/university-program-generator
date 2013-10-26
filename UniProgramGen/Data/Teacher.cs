4dbcbbee20e6adc805be6cf1d23a657158a5d293
David Tankov <lordmordevil@gmail.com>
2013-10-26 15:41:40 +0300

    Remove subjects from Teacher class

--- UniProgramGen/Data/Teacher.cs
+++ UniProgramGen/Data/Teacher.cs
@@ -5,12 +5,10 @@ namespace UniProgramGen.Data
     public class Teacher
     {
         // TODO: add list of requirements (as Requirement)
-        public readonly List<Subject> subjects;
         public readonly string name;
 
-        public Teacher(List<Subject> subjects, string name)
+        public Teacher(string name)
         {
-            this.subjects = subjects;
             this.name = name;
         }
     }
