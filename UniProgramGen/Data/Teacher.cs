3a4dced46edfba9815692b359ec7500f40724952
Victoria Hristova <viki.hristova@gmail.com>
2013-10-26 17:18:48 +0300

    Add get method for teacher name

--- UniProgramGen/Data/Teacher.cs
+++ UniProgramGen/Data/Teacher.cs
@@ -7,6 +7,11 @@ namespace UniProgramGen.Data
         public Requirements requirements { get; internal set; }
         public string name { get; internal set; }
 
+        public string Name
+        {
+            get { return name; }
+        }
+
         public Teacher(Requirements requirements, string name)
         {
             this.requirements = requirements;
