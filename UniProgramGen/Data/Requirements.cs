6e580afe0e0cf234b58fd43abc0c8199644904a6
David Tankov <lordmordevil@gmail.com>
2013-10-26 16:10:35 +0300

    Rename requiredTimeSlots to availableTimeSlots

--- UniProgramGen/Data/Requirements.cs
+++ UniProgramGen/Data/Requirements.cs
@@ -5,13 +5,13 @@ namespace UniProgramGen.Data
     public class Requirements
     {
         public readonly double weight;
-        public readonly List<TimeSlot> requiredTimeSlots;
+        public readonly List<TimeSlot> availableTimeSlots;
         public readonly List<Room> requiredRooms;
 
-        public Requirements(double weight, List<TimeSlot> requiredTimeSlots, List<Room> requiredRooms)
+        public Requirements(double weight, List<TimeSlot> availableTimeSlots, List<Room> requiredRooms)
         {
             this.weight = weight;
-            this.requiredTimeSlots = requiredTimeSlots;
+            this.availableTimeSlots = availableTimeSlots;
             this.requiredRooms = requiredRooms;
         }
     }
