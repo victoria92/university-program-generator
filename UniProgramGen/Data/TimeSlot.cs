747f4f43fa6ff91994b39303f6e3aba8ba81e37f
David Tankov <lordmordevil@gmail.com>
2013-10-26 16:10:49 +0300

    Add dayOfWeek to TimeSlot

--- UniProgramGen/Data/TimeSlot.cs
+++ UniProgramGen/Data/TimeSlot.cs
@@ -6,11 +6,13 @@ namespace UniProgramGen.Data
     {
         public readonly DateTime startTime;
         public readonly DateTime endTime;
+        public readonly byte dayOfWeek;
 
-        public TimeSlot(DateTime startTime, DateTime endTime)
+        public TimeSlot(DateTime startTime, DateTime endTime, byte dayOfWeek)
         {
             this.startTime = startTime;
             this.endTime = endTime;
+            this.dayOfWeek = dayOfWeek;
         }
     }
 }
