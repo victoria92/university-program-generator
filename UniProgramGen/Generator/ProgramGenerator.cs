c64ccb930222d4c73ed4d4575e84f39344f572e2
David Tankov <lordmordevil@gmail.com>
2013-10-26 16:42:09 +0300

    Add initial commit for ProgramGenerator

---
+++ UniProgramGen/Generator/ProgramGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniProgramGen.Data;
+
+namespace UniProgramGen.Generator
+{
+    public class ProgramGenerator
+    {
+        public void GenerateProgram(List<Room> rooms, List<Subject> subjects, List<Teacher> teachers, List<Group> groups)
+        {
+            CalculateRequirementInternalWeights(rooms.Count, teachers.Select(t => t.requirements));
+
+            //teachers.Sort((teacher1, teacher2) => teacher1.requirements < teacher2.requirements);
+        }
+
+        private void CalculateRequirementInternalWeights(int roomsLength, IEnumerable<Requirements> requirements)
+        {
+            foreach (var requirement in requirements)
+            {
+                requirement.CalculateInternalWeight(roomsLength);
+            }
+        }
+    }
+
+}
