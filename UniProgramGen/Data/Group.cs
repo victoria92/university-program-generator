3947c67e72d43d41e57095d17111211cc45bc834
David Tankov <lordmordevil@gmail.com>
2013-10-26 15:24:00 +0300

    Fix wrong generic arguments

--- UniProgramGen/Data/Group.cs
+++ UniProgramGen/Data/Group.cs
@@ -4,11 +4,11 @@ namespace UniProgramGen.Data
 {
     public class Group
     {
-        public readonly List<Room> horarium;
+        public readonly List<Subject> horarium;
         public readonly uint size;
         public readonly string name;
 
-        public Group(List<Room> horarium, uint size, string name)
+        public Group(List<Subject> horarium, uint size, string name)
         {
             this.horarium = horarium;
             this.size = size;
--- UniProgramGen/Data/Teacher.cs
+++ UniProgramGen/Data/Teacher.cs
@@ -4,12 +4,11 @@ namespace UniProgramGen.Data
 {
     public class Teacher
     {
-        // TODO: add list of requirements
-
-        public readonly List<Room> subjects;
+        // TODO: add list of requirements (as Requirement)
+        public readonly List<Subject> subjects;
         public readonly string name;
 
-        public Teacher(List<Room> subjects, string name)
+        public Teacher(List<Subject> subjects, string name)
         {
             this.subjects = subjects;
             this.name = name;
