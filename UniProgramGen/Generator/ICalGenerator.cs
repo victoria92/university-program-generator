8530d5e1048ee7a1bd798953cc2514304432c839
Valentin Aitken <bostko@gmail.com>
2013-10-27 20:46:54 +0200

    ff

--- UniProgramGen/Generator/ICalGenerator.cs
+++ UniProgramGen/Generator/ICalGenerator.cs
@@ -20,7 +20,7 @@ namespace UniProgramGen.Generator
         {
             DDay.iCal.iCalendar iCal = new DDay.iCal.iCalendar();
             var t = new DBManager();
-            t.initOutputData();
+            t.initInputData();
             t.initOutputData();
             foreach(ScheduledTimeSlot[] solution in (t.solutions))
             {
