e29fbafa0a535a443da1b00753645bee7d740f67
Joan Karadimov <joan.karadimov@gmail.com>
2013-10-27 21:27:56 +0200

    Add a popup with an entity's schedule

---
+++ UniProgramGen/FormSchedule.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace UniProgramGen
+{
+    partial class FormSchedule
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(653, 298);
+            this.webBrowser.TabIndex = 0;
+            // 
+            // FormSchedule
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 335);
+            this.Controls.Add(this.webBrowser);
+            this.Name = "FormSchedule";
+            this.Text = "Разписание";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
---
+++ UniProgramGen/FormSchedule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace UniProgramGen
+{
+    public partial class FormSchedule : Form
+    {
+        public FormSchedule()
+        {
+            InitializeComponent();
+
+            string curDir = Directory.GetCurrentDirectory();
+            string exampleDataPath = String.Format("file:///{0}/../../datafiles/example_solution.html", curDir);
+            this.webBrowser.Url = new Uri(exampleDataPath);
+        }
+    }
+}
