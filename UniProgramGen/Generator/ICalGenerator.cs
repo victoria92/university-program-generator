e53949acba423388cf062aa52026871d74b73dec
Valentin Aitken <bostko@gmail.com>
2013-10-27 08:48:34 +0200

    Some exapmle code...

---
+++ UniProgramGen/Generator/ICalGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace UniProgramGen.Generator
+{
+    public class ICalGenerator
+    {
+        byte[] buffer;
+        
+        public ICalGenerator()
+        {
+
+        }
+
+        public void readGeneratedJSON()
+        {
+            string json_output = File.ReadAllText(@"db\program_is.json");
+            Console.WriteLine(json_output);
+
+        }
+    }
+}
---
+++ UnitTests/ICalGenUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniProgramGen.Generator;
+
+namespace UnitTests
+{
+    class ICalGenUnitTests
+    {
+        [TestMethod]
+        public void testReadingFile()
+        {
+            (new ICalGenerator()).readGeneratedJSON();
+        }
+    }
+}
