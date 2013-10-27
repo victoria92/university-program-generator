e8793c625d35db20508fac15c1fde550ffbcb94c
Victoria Hristova <viki.hristova@gmail.com>
2013-10-27 12:13:23 +0200

    Room add fixes

---
+++ UniProgramGen/Helpers/UIHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace UniProgramGen.Helpers
+{
+    public static class UIHelpers
+    {
+        public static List<TimeSlot> getListBoxRoomDayTimeSlot(ListBox listBox, DayOfWeek dayOfWeek)
+        {
+            uint startHour = 0;
+            uint endHour = 0;
+            uint previousHour = 0;
+
+            List<TimeSlot> result = new List<TimeSlot>();
+
+            foreach (string selectedItem in listBox.SelectedItems)
+            {
+                uint hour = Convert.ToUInt32(selectedItem.Remove(selectedItem.Length - 3));
+                if (hour != previousHour + 1) // new Timeslot
+                {
+                    if (startHour != 0)
+                    {
+                        result.Add(new TimeSlot(dayOfWeek, startHour, endHour));
+                    }
+                    startHour = hour;
+                }
+
+                endHour = hour + 1;
+                previousHour = hour;
+            }
+            return result;
+        }
+    }
+}
--- UniProgramGen/RoomsTab.cs
+++ UniProgramGen/RoomsTab.cs
@@ -24,42 +24,16 @@ namespace UniProgramGen
         {
         }
 
-        private List<TimeSlot> getListBoxRoomDayTimeSlot(ListBox listBox, DayOfWeek dayOfWeek)
-        {
-            uint startHour = 0;
-            uint endHour = 0;
-            uint previousHour = 0;
-
-            List<TimeSlot> result = new List<TimeSlot>();
-
-            foreach (string selectedItem in listBox.SelectedItems)
-            {
-                uint hour = Convert.ToUInt32(selectedItem.Remove(selectedItem.Length - 3));
-                if (hour != previousHour + 1) // new Timeslot
-                {
-                    if (startHour != 0)
-                    {
-                        result.Add(new TimeSlot(dayOfWeek, startHour, endHour));
-                    }
-                    startHour = hour;
-                }
-
-                endHour = hour + 1;
-                previousHour = hour;
-            }
-            return result;
-        }
-
         public List<Room> Rooms { get; private set; }
 
         private void buttonAddRoom_Click(object sender, EventArgs e)
         {
-            var mondayAvailableTime = getListBoxRoomDayTimeSlot(listBoxRoomMonday, DayOfWeek.Monday);
-            var tuesdayAvailableTime = getListBoxRoomDayTimeSlot(listBoxRoomTuesday, DayOfWeek.Monday);
-            var wednesdayAvailableTime = getListBoxRoomDayTimeSlot(listBoxRoomWednesday, DayOfWeek.Monday);
-            var thursdayAvailableTime = getListBoxRoomDayTimeSlot(listBoxRoomThursday, DayOfWeek.Monday);
-            var fridayAvailableTime = getListBoxRoomDayTimeSlot(listBoxRoomFriday, DayOfWeek.Monday);
-            var saturdayAvailableTime = getListBoxRoomDayTimeSlot(listBoxRoomSaturday, DayOfWeek.Monday);
+            var mondayAvailableTime = UIHelpers.getListBoxRoomDayTimeSlot(listBoxRoomMonday, DayOfWeek.Monday);
+            var tuesdayAvailableTime = UIHelpers.getListBoxRoomDayTimeSlot(listBoxRoomTuesday, DayOfWeek.Monday);
+            var wednesdayAvailableTime = UIHelpers.getListBoxRoomDayTimeSlot(listBoxRoomWednesday, DayOfWeek.Monday);
+            var thursdayAvailableTime = UIHelpers.getListBoxRoomDayTimeSlot(listBoxRoomThursday, DayOfWeek.Monday);
+            var fridayAvailableTime = UIHelpers.getListBoxRoomDayTimeSlot(listBoxRoomFriday, DayOfWeek.Monday);
+            var saturdayAvailableTime = UIHelpers.getListBoxRoomDayTimeSlot(listBoxRoomSaturday, DayOfWeek.Monday);
 
             List<TimeSlot> roomAvailability = new List<TimeSlot>();
             roomAvailability.AddRange(mondayAvailableTime);
