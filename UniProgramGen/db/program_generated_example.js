9261360db410790fc7bcc63fa7141f7431776d6e
Valentin Aitken <bostko@gmail.com>
2013-10-27 10:09:11 +0200

    The structure of the program we intent to generate

---
+++ UniProgramGen/db/program_generated_example.js
@@ -0,0 +1,85 @@
+{
+    "group_name": "Информатика - Група 4",
+    "program": [
+        [{
+            "label": "ДИС  I",
+            "room": "326",
+            "start": 9,
+            "end": 11,
+            "type": "lecture",
+            "teacher": "доц. М.Такев"
+        }, {
+            "label": "ДисСтр",
+            "room": "326",
+            "start": 11,
+            "end": 14,
+            "type": "lecture",
+            "teacher": "доц. С.Николова"
+        }, {
+            "label": "ДИС I",
+            "room": "ФХФ-604",
+            "start": 14,
+            "end": 16,
+            "type": "lab"
+        }],
+
+        [{
+            "label": "ДИС  I",
+            "room": "02",
+            "start": 9,
+            "end": 11,
+            "type": "lecture",
+            "teacher": "доц. М.Такев"
+        }, {
+            "label": "Алг",
+            "room": "308",
+            "start": 11,
+            "end": 14,
+            "type": "lab"
+        }, {
+            "label": "ДИС I",
+            "room": "308",
+            "start": 14,
+            "end": 17,
+            "type": "lab"
+        }],
+
+        [{
+            "label": "ДисСтр",
+            "room": "405",
+            "start": 12,
+            "end": 15,
+            "type": "lab"
+        }, {
+            "label": "Алгебра",
+            "room": "326",
+            "start": 15,
+            "end": 18,
+            "type": "lecture",
+            "teacher": "гл.асист. Н.Колев"
+        }],
+
+        [],
+
+        [{
+            "label": "УП",
+            "room": "326",
+            "start": 9,
+            "end": 12,
+            "type": "lecture",
+            "teacher": "доц. А. Семерджиев"
+        }, {
+            "label": "УП",
+            "room": "314к",
+            "start": 12,
+            "end": 14,
+            "type": "lab"
+        }, {
+            "label": "ид  УП(Практ)",
+            "room": "321к",
+            "start": 14,
+            "end": 16,
+            "type": "lab"
+        }]
+    ]
+}
