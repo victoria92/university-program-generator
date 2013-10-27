74e4626b00535fb7e6808b28d27d5458c5026b95
Valentin Aitken <bostko@gmail.com>
2013-10-27 13:11:16 +0200

    RoomTypes dumped as string:

--- UniProgramGen/Data/RoomType.cs
+++ UniProgramGen/Data/RoomType.cs
@@ -1,6 +1,9 @@
 ﻿
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 namespace UniProgramGen.Data
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum RoomType
     {
         // TODO: add more types
